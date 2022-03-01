4745cfe29fdfb44751f2a79ba8e7676fd4dcd524
mengiefen <mengiefen@gmail.com>
2022-03-01 21:35:26 +0300

    add a function to listen click and load events

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,1 +1,31 @@
 import '../css/styles.css';
+import SCORE from './scores.js';
+import renderPage from './render-page.js';
+
+const getFormData = () => {
+  const name = document.querySelector('#name');
+  const score = document.querySelector('#score');
+  if (name.value !== '' && score.value !== '') {
+    const Score = new SCORE(name.value, score.value);
+    Score.sendScores();
+    return true;
+  }
+  return false;
+};
+
+const eventHandler = (eventType, selector, callback) => {
+  document.addEventListener(eventType, (e) => {
+    if (e.target.matches(selector) && true) callback(e);
+  });
+};
+
+eventHandler('click', '#story-submit', (e) => {
+  e.preventDefault();
+  const form = document.querySelector('.story-submit-form');
+  if (getFormData()) {
+    renderPage();
+    form.reset();
+  }
+});
+
+document.addEventListener('DOMContentLoaded', renderPage);
