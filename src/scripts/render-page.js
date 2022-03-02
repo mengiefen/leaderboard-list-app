b3764bec87aabb28aebcc8c07aa5a2e391880bbf
mengiefen <mengiefen@gmail.com>
2022-03-02 23:23:32 +0300

    update the functions to populate from fetched data

--- src/scripts/render-page.js
+++ src/scripts/render-page.js
@@ -2,13 +2,13 @@ const createScoreRow = (score) => {
   const listRow = document.createElement('div');
   const nameHolder = document.createElement('p');
   const scoreHolder = document.createElement('p');
-  const classLists = ['story-list-row', 'story-list-name', 'story-list-score'];
+  const classLists = ['score-list-row', 'score-list-name', 'score-list-score'];
 
   [listRow, nameHolder, scoreHolder].forEach((element, index) => {
     element.setAttribute('class', classLists[index]);
   });
 
-  nameHolder.textContent = score.name;
+  nameHolder.textContent = score.user;
   scoreHolder.textContent = score.score;
 
   listRow.append(nameHolder, scoreHolder);
@@ -17,21 +17,21 @@ const createScoreRow = (score) => {
 };
 
 const alternatBackground = () => {
-  const rows = [...document.querySelectorAll('.story-list-row')];
+  const rows = [...document.querySelectorAll('.score-list-row')];
   rows.forEach((row, index) => {
-    if (index % 2 === 0) row.style.backgroundColor = '#ddd';
+    if (index % 2 === 0) row.style.backgroundColor = 'rgba(0,0,0,0.1)';
   });
 };
 
 const addChild = (score) => {
-  const leaderList = document.querySelector('.story-list-box');
+  const leaderList = document.querySelector('.score-list-box');
   leaderList.appendChild(createScoreRow(score));
   alternatBackground();
 };
 
-const renderPage = () => {
-  const leaderList = document.querySelector('.story-list-box');
-  const scores = JSON.parse(sessionStorage.getItem('scores'));
+const renderPage = (scores) => {
+  const leaderList = document.querySelector('.score-list-box');
+  leaderList.innerHTML = '';
   scores.forEach((score) => {
     leaderList.appendChild(createScoreRow(score));
   });
